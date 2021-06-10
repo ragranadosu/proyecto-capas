e02acbb2748d687890fef8086aaed0f259481295
Raúl Granados <raul.granados12@gmail.com>
2021-06-10 15:16:31 -0600

    Agregando usuario a capa datos

---
+++ SQL/datos.sql
@@ -0,0 +1,3 @@
+use proyecto;
+
+INSERT INTO usuario (nombre, apellido, email, contrasena, rol)  values ('Raul', 'Granados', 'raulmail@mail.com', 'pass', 'profesor');
--- Solucion/Proyecto N Capas/Poryecto.Datos/DUsuario.cs
+++ Solucion/Proyecto N Capas/Poryecto.Datos/DUsuario.cs
@@ -1,10 +1,8 @@
-﻿using Proyecto.Entidades;
+﻿using Poryecto.Datos;
+using Proyecto.Entidades;
 using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace Proyecto.Datos
 {
@@ -12,12 +10,61 @@ namespace Proyecto.Datos
     {
         public DataTable Listar()
         {
+            SqlDataReader Resultado;
 
+            DataTable Tabla = new DataTable();
+
+            SqlConnection SqlCon = new SqlConnection();
+
+            try
+            {
+                SqlCon = Conexion.getInstancia().CrearConexion();
+
+                SqlCommand Comando = new SqlCommand("usuario_listar", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                SqlCon.Open();
+                Resultado = Comando.ExecuteReader();
+                Tabla.Load(Resultado);
+                return Tabla;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally 
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close(); 
+            }
         }
 
-        public DataTable Buscar(string valor)
+        public DataTable Buscar(string Valor)
         {
+            SqlDataReader Resultado;
 
+            DataTable Tabla = new DataTable();
+
+            SqlConnection SqlCon = new SqlConnection();
+
+            try
+            {
+                SqlCon = Conexion.getInstancia().CrearConexion();
+
+                SqlCommand Comando = new SqlCommand("usuario_buscarr", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@valor", SqlDbType.VarChar).Value = Valor;
+                SqlCon.Open();
+                Resultado = Comando.ExecuteReader();
+                Tabla.Load(Resultado);
+                return Tabla;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
         }
     }
 }
