3965ddf56f644be1d0899d376afd47a9b2afb360
Raúl Granados <raul.granados12@gmail.com>
2021-06-09 18:27:41 -0600

    Agregando entidad Uusuario

--- Solucion/Proyecto N Capas/Poryecto.Entidades/Libro.cs
+++ Solucion/Proyecto N Capas/Poryecto.Entidades/Libro.cs
@@ -8,5 +8,30 @@ namespace Proyecto.Entidades
 {
     class Libro
     {
+        public int IdLibro { get; set; }
+
+        public int Num_Ejemplares { get; set; }
+
+        public string Isbn { get; set; }
+
+        public string Autor { get; set; }
+
+        public string Editorial { get; set; }
+
+        public string Anio_Edicion { get; set; }
+
+        public int Num_Edicion { get; set; }
+
+        public string Pais { get; set; }
+
+        public string Idioma { get; set; }
+
+        public string Materia { get; set; }
+
+        public string Num_Paginas { get; set; }
+
+        public string Ubicacion { get; set; }
+
+        public string Descripcion { get; set; }
     }
 }
